8bdf4c0b3ccdc864bcdd35b387cd15a3c56cf3ca
ayhm23 <145286298+ayhm23@users.noreply.github.com>
2025-02-14 22:22:01 +0530

    Time: 349 ms (11.88%), Space: 61.9 MB (13.18%) - LeetHub

---
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestSubstring(string s, int k) {
+        if (s.empty()) return 0;
+
+        unordered_map<char, int> freq;
+        for (char c : s) freq[c]++;
+
+        // Find first "bad" character that appears less than k times
+        for (int i = 0; i < s.size(); i++) {
+            if (freq[s[i]] < k) {
+                // Split the string at the breaking point
+                int left = longestSubstring(s.substr(0, i), k);
+                int right = longestSubstring(s.substr(i + 1), k);
+                return max(left, right);
+            }
+        }
+
+        // If there is no breaking point, the entire string is valid
+        return s.size();
+    }
+};
