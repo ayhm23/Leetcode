75ef825a3ad57a70ad5ec184a3ab73f24cfb5f27
ayhm23 <145286298+ayhm23@users.noreply.github.com>
2025-03-26 17:47:34 +0530

    Time: 0 ms (100%), Space: 17.5 MB (27.5%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -2,30 +2,30 @@ class Solution {
 public:
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> color(n, -1);  // -1: Unvisited, 0: Red, 1: Blue
+        vector<int> color(n, -1); // -1: unvisited, 0 & 1: two colors
         
-        for (int i = 0; i < n; i++) {
-            if (color[i] != -1) continue;  // Skip already colored components
-            
-            queue<int> q;
-            q.push(i);
-            color[i] = 0;  // Start coloring from 0
-            
-            while (!q.empty()) {
-                int node = q.front();
-                q.pop();
-                
-                for (int neighbor : graph[node]) {
-                    if (color[neighbor] == -1) {
-                        color[neighbor] = !color[node];  // Assign opposite color
-                        q.push(neighbor);
-                    } 
-                    else if (color[neighbor] == color[node]) {
-                        return false;  // Conflict detected
+        for(int i = 0; i < n; i++) {
+            if(color[i] == -1) { // Only process unvisited components
+                queue<int> q;
+                q.push(i);
+                color[i] = 0; // Start with color 0
+
+                while(!q.empty()) {
+                    int node = q.front();
+                    q.pop();
+
+                    for(int neighbour : graph[node]) {
+                        if(color[neighbour] == -1) { // Only color unvisited nodes
+                            color[neighbour] = !color[node]; // Assign opposite color
+                            q.push(neighbour);
+                        } 
+                        else if(color[neighbour] == color[node]) { 
+                            return false; // Conflict detected
+                        }
                     }
                 }
             }
         }
-        return true;  // Graph is bipartite
+        return true;
     }
 };
