ba36219b239e9b9ccebdf8a6b9b1b7111e5d9111
ayhm23 <145286298+ayhm23@users.noreply.github.com>
2025-03-27 10:42:49 +0530

    Time: 1 ms (84.06%), Space: 18.1 MB (73.05%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,41 @@
+
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        vector<int> inDegree(numCourses, 0);
+        
+        // Build the adjacency list and calculate in-degrees
+        for (auto& pre : prerequisites) {
+            int course = pre[0];
+            int prereq = pre[1];
+            adj[prereq].push_back(course);
+            inDegree[course]++;
+        }
+        
+        // Queue for courses with no prerequisites
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegree[i] == 0) {
+                q.push(i);
+            }
+        }
+        
+        vector<int> order;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            order.push_back(node);
+            
+            for (int neighbor : adj[node]) {
+                inDegree[neighbor]--;
+                if (inDegree[neighbor] == 0) {
+                    q.push(neighbor);
+                }
+            }
+        }
+        
+        // If we have processed all courses, return the order; otherwise, return an empty array
+        return (order.size() == numCourses) ? order : vector<int>();
+    }
+};
