1aaf3de5278a051e721648a2aaf06f6c0b195713
ayhm23 <145286298+ayhm23@users.noreply.github.com>
2025-05-31 15:03:47 +0530

    Time: 0 ms (100%), Space: 22.6 MB (50.5%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -3,13 +3,13 @@ public:
     int removeDuplicates(vector<int>& nums) {
         if (nums.empty()) return 0;
 
-        int write = 1;                 // next position to write a new unique value
+        int write = 1;
         for (int read = 1; read < nums.size(); ++read) {
-            if (nums[read] != nums[read - 1]) {   // new unique value found
-                nums[write] = nums[read];         // place it at the write pointer
+            if (nums[read] != nums[read - 1]) {
+                nums[write] = nums[read];
                 ++write;
             }
         }
-        return write;      // number of unique elements
+        return write;
     }
 };
