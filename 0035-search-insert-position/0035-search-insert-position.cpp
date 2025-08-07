1fda27594b8faaa0109a13a7e54a296ef187ae6a
ayhm23 <145286298+ayhm23@users.noreply.github.com>
2025-08-07 12:20:06 +0530

    Time: 0 ms (100%), Space: 13.7 MB (41.86%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int bs(vector<int> &nums, int target, int l, int r) {
+        if (l > r) return l; // insert position
+
+        int mid = l + (r - l) / 2;
+
+        if (nums[mid] == target) return mid;
+        else if (nums[mid] > target)
+            return bs(nums, target, l, mid - 1);
+        else
+            return bs(nums, target, mid + 1, r);
+    }
+
+    int searchInsert(vector<int>& nums, int target) {
+        return bs(nums, target, 0, nums.size() - 1);   
+    }
+};
