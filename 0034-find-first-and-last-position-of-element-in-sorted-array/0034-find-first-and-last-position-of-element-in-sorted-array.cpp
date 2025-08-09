3ff0194010557517f8ea67c626b02346c8fca49f
ayhm23 <145286298+ayhm23@users.noreply.github.com>
2025-08-09 18:54:42 +0530

    Time: 0 ms (100%), Space: 18 MB (17.11%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -4,34 +4,32 @@ public:
         vector<int> ans = {-1, -1};
         int x = target;
         int n = nums.size();
-        int low = 0, high = n-1;
-        while(low <= high){
-            int mid = low + (high-low)/2;
-            if(nums[mid] == x){
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == x) {
                 ans[0] = mid;
                 high = mid - 1;
             }
 
-            if(nums[mid] < x){
+            if (nums[mid] < x) {
                 low = mid + 1;
-            }
-            else if(nums[mid] > x){
+            } else if (nums[mid] > x) {
                 high = mid - 1;
             }
         }
-         low = 0, high = n-1;
-        
-        while(low <= high){
-            int mid = low + (high-low)/2;
-            if(nums[mid] == x){
+        low = 0, high = n - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == x) {
                 ans[1] = mid;
                 low = mid + 1;
             }
 
-            if(nums[mid] < x){
+            if (nums[mid] < x) {
                 low = mid + 1;
-            }
-            else if(nums[mid] > x){
+            } else if (nums[mid] > x) {
                 high = mid - 1;
             }
         }
