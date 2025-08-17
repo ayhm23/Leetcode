8c5b24dce78e9c95ca9ee0d61499ea110aed0303
ayhm23 <145286298+ayhm23@users.noreply.github.com>
2025-08-17 21:52:44 +0530

    Time: 0 ms (100%), Space: 28.2 MB (28.34%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,44 +1,14 @@
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
-    // Function to count occurrences of 'num' in the subarray [left, right]
-    int countOccurrences(vector<int>& nums, int left, int right, int num) {
-        int count = 0;
-        for (int i = left; i <= right; i++) {
-            if (nums[i] == num) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    int majorElement(vector<int>& nums, int left, int right) {
-        if (left == right) {
-            return nums[left];
-        }
-
-        // Corrected midpoint calculation
-        int mid = left + (right - left) / 2;
-
-        // Recursively find majority elements in left and right halves
-        int leftMajor = majorElement(nums, left, mid);
-        int rightMajor = majorElement(nums, mid + 1, right);
-
-        // If both halves agree on the majority element, return it
-        if (leftMajor == rightMajor) {
-            return leftMajor;
-        }
-
-        // Count occurrences in the current range
-        int leftCount = countOccurrences(nums, left, right, leftMajor);
-        int rightCount = countOccurrences(nums, left, right, rightMajor);
-
-        return (leftCount > rightCount) ? leftMajor : rightMajor;
-    }
-
     int majorityElement(vector<int>& nums) {
-        return majorElement(nums, 0, nums.size() - 1);
+        int candidate = 0, count = 0;
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+        return candidate;
     }
+
 };
