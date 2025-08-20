b07b3a5d6db0f3591f2d09f28caa2ad010ac2207
ayhm23 <145286298+ayhm23@users.noreply.github.com>
2025-08-20 10:17:47 +0530

    Time: 1 ms (40.54%), Space: 95.2 MB (61.39%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,19 +1,59 @@
+// class Solution {
+// public:
+//     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+//         // ensure nums1 is the smaller array
+//         if (nums1.size() > nums2.size()) 
+//             return findMedianSortedArrays(nums2, nums1);
+
+//         int n1 = nums1.size(), n2 = nums2.size();
+//         int total = n1 + n2;
+//         int half = (total + 1) / 2;
+
+//         int lo = 0, hi = n1;
+
+//         while (lo <= hi) {
+//             int cut1 = (lo + hi) / 2;       // partition in nums1
+//             int cut2 = half - cut1;         // partition in nums2
+
+//             int l1 = (cut1 == 0) ? INT_MIN : nums1[cut1 - 1];
+//             int l2 = (cut2 == 0) ? INT_MIN : nums2[cut2 - 1];
+//             int r1 = (cut1 == n1) ? INT_MAX : nums1[cut1];
+//             int r2 = (cut2 == n2) ? INT_MAX : nums2[cut2];
+
+//             if (l1 <= r2 && l2 <= r1) {
+//                 if (total % 2 == 0) {
+//                     return (max(l1, l2) + min(r1, r2)) / 2.0;
+//                 } else {
+//                     return max(l1, l2);
+//                 }
+//             }
+//             else if (l1 > r2) {
+//                 hi = cut1 - 1;
+//             }
+//             else {
+//                 lo = cut1 + 1;
+//             }
+//         }
+//         return 0.0; // never reached
+//     }
+// };
+
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        // ensure nums1 is the smaller array
         if (nums1.size() > nums2.size()) 
             return findMedianSortedArrays(nums2, nums1);
 
         int n1 = nums1.size(), n2 = nums2.size();
-        int total = n1 + n2;
-        int half = (total + 1) / 2;
-
         int lo = 0, hi = n1;
+        int totalLeft = (n1 + n2 + 1) / 2; // total elements on left side
 
         while (lo <= hi) {
-            int cut1 = (lo + hi) / 2;       // partition in nums1
-            int cut2 = half - cut1;         // partition in nums2
+            int cut1 = (lo + hi) / 2;
+            int cut2 = totalLeft - cut1;
 
             int l1 = (cut1 == 0) ? INT_MIN : nums1[cut1 - 1];
             int l2 = (cut2 == 0) ? INT_MIN : nums2[cut2 - 1];
@@ -21,19 +61,19 @@ public:
             int r2 = (cut2 == n2) ? INT_MAX : nums2[cut2];
 
             if (l1 <= r2 && l2 <= r1) {
-                if (total % 2 == 0) {
+                if ((n1 + n2) % 2 == 0) {
                     return (max(l1, l2) + min(r1, r2)) / 2.0;
                 } else {
                     return max(l1, l2);
                 }
-            }
+            } 
             else if (l1 > r2) {
                 hi = cut1 - 1;
-            }
+            } 
             else {
                 lo = cut1 + 1;
             }
         }
-        return 0.0; // never reached
+        return 0.0; // should never reach here
     }
 };
