15f0bfc8ab177fa1486459e0366e2d2278d99404
ayhm23 <145286298+ayhm23@users.noreply.github.com>
2025-08-21 23:52:02 +0530

    Time: 0 ms (100%), Space: 10.1 MB (85.19%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -5,15 +5,12 @@ public:
         int i = s.size() - 1;
         
         while(i >= 0){
-            // skip trailing spaces
             while(i >= 0 && s[i] == ' ') i--;
             if(i < 0) break;
             
             int j = i;
-            // move left until space or start
             while(i >= 0 && s[i] != ' ') i--;
             
-            // take substring [i+1 .. j]
             if(!ans.empty()) ans.push_back(' ');
             ans.append(s.substr(i+1, j-i));
         }
