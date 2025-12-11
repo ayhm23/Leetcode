f84e763e21375f98cc8646ac2b71f5191bf3dea9
ayhm23 <145286298+ayhm23@users.noreply.github.com>
2025-12-11 14:30:56 +0530

    Time: 0 ms (100%), Space: 14.9 MB (19.94%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-    unordered_map<int, int> mp;  // value -> index
-    for (int i = 0; i < nums.size(); i++) {
-        int complement = target - nums[i];
-        if (mp.find(complement) != mp.end()) {
-            return {mp[complement], i};  // found the pair
+        // approach 1.. two nested look.. i and i to n -1 O(n^2) O(1)
+        // approach 2.. hashing (index, value)initially.. look for target - nums[i] in hashmap...O(N) and O(N)
+        // approach 3.. vector<pair(value,index)> sort by values.. for ith value use binary search for target - nums[i] O(NlogN) O(N)
+
+        unordered_map<int,int> map;
+        
+        for(int i = 0; i < nums.size(); i++){
+            int x = target - nums[i];
+
+            if(map.find(x) != map.end()){
+                return {map[x], i};
+            }
+            map[nums[i]] = i;
         }
-        mp[nums[i]] = i;
+    return {};
     }
-    return {}; // not expected (as per problem statement)
-}
 };
