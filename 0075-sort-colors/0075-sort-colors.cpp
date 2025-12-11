7478b178ce2c51e60b1ea1156595c0659359dc20
ayhm23 <145286298+ayhm23@users.noreply.github.com>
2025-12-11 17:08:59 +0530

    Time: 0 ms (100%), Space: 11.7 MB (48.92%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -3,6 +3,7 @@ public:
     void sortColors(vector<int>& nums) {
         //a1 : count all 0s 1s 2s and give array accordnigly
         //a2 : left right and read pointer
+        //Dutch flag colour algorithm
 
         int n = nums.size();
         int left = 0; int right = n-1;
