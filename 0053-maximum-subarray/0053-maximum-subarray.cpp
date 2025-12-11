a799495c9f55321ef354dcf36d55c52ed11d9bd6
ayhm23 <145286298+ayhm23@users.noreply.github.com>
2025-12-11 23:09:00 +0530

    Time: 0 ms (100%), Space: 71.5 MB (97.82%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,29 +1,18 @@
 class Solution {
 public:
+    int maxSubArray(vector<int>& nums) {
+        //keep best and current count in kadane's algo
+        //approach 1 : BF : make all possible subarrays O(n^2) O(1)
+        //approach 2 : Kadane's algo
 
-    //2 methods of solving...first is Dp
-    //Dp better..Divide conquer complex
+        long long best = nums[0];
+        long long curr = nums[0];
 
-    int maxSubArray(vector<int>& nums){
-         return KadaneS_Algo(nums);
-
-        
-
-    }
-    
-    //Kadane's Algorithm
-    int KadaneS_Algo(vector<int>& nums) {
-        int maxSum = nums[0], currentSum = 0;
-        for(int num : nums){
-            currentSum+=num;
-            maxSum = max(currentSum,maxSum);
-
-            if(currentSum < 0)currentSum = 0;
-
+        for (int i = 1; i < nums.size(); ++i) {
+            curr = max((long long)nums[i], curr + (long long)nums[i]);
+            best = max(best, curr);
         }
-        return maxSum;
+
+        return (int)best;
     }
-
-    
-
 };
