ecc0f2e4d02f5e45128e6dfaa904381a4acfa4a9
ayhm23 <145286298+ayhm23@users.noreply.github.com>
2025-12-11 17:01:34 +0530

    Time: 0 ms (100%), Space: 28.2 MB (28.13%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int candidate = 0, count = 0;
-        for (int num : nums) {
-            if (count == 0) {
-                candidate = num;
-            }
-            count += (num == candidate) ? 1 : -1;
-        }
-        return candidate;
-    }
+        //approach 1 : BF : iterate through array and count occurance with nester loop - O(n^2) O(1)
+        //approach 2 : hashmap : count eash occurance..itereate through hashmap to find the highest occuring element = O(N) O(N)
+        //approach 3 : make use of the always exisiting statement..keep counter.. if counter zero.. count new element..
 
+        int count = 1 , element = nums[0];
+        int n = nums.size();
+
+        for(int i = 1; i < n; i++){
+            if(nums[i] == element){
+                count++;
+            }
+            else{
+                if(count == 0){
+                    count = 1;
+                    element = nums[i];
+                }
+                else{
+                    count--;
+                }
+            }
+        }
+        return element;
+    }
 };
