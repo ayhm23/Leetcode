1f7c58abbb75d7214963491a52bd15877f3f1afd
ayhm23 <145286298+ayhm23@users.noreply.github.com>
2025-12-11 16:05:54 +0530

    Time: 0 ms (100%), Space: 11.7 MB (48.92%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        //a1 : count all 0s 1s 2s and give array accordnigly
+        //a2 : left right and read pointer
+
+        int n = nums.size();
+        int left = 0; int right = n-1;
+        int reader = 0;
+        //left >=  reader and right <= reader
+        while(reader <= right){
+            if(nums[reader] == 0){
+                swap(nums[left], nums[reader]);
+                left++; reader++;
+            }
+            else if(nums[reader] == 1){
+                reader++;
+            }
+            else if(nums[reader] == 2){
+                swap(nums[right], nums[reader]);
+                right--;
+            }
+        }
+    }
+};
