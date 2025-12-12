2c1c10f5b2650f8393cd3987cbd7267238a7bc30
ayhm23 <145286298+ayhm23@users.noreply.github.com>
2025-12-12 22:55:05 +0530

    Time: 0 ms (100%), Space: 9.5 MB (90.8%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length()) return false;
+        //sol 1 : sort both and compare
+        //sol 2 : maintain frequeny of the 26 character O(N) O(1)
+        if(s.size() != t.size()) return false;
 
-        unordered_map<char, int> mapS, mapT;
+        int freq[26] = {0};
 
-        for (int i = 0; i < s.length(); i++) {
-            mapS[s[i]]++;
-            mapT[t[i]]++;
+        for(char c : s) freq[c - 'a']++;
+        for(char c : t) freq[c - 'a']--;
+
+        for(int i = 0; i < 26; i++){
+            if(freq[i] != 0) return false;
         }
-
-        return mapS == mapT;
+        return true;
     }
 };
