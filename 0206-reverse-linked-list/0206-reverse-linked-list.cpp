faaac3f43f232137413c419bfc92979f54a248b8
ayhm23 <145286298+ayhm23@users.noreply.github.com>
2026-01-16 23:08:33 +0530

    Time: 0 ms (100%), Space: 13.2 MB (90.97%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -1,16 +1,37 @@
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         ListNode* prev = NULL;
+//         ListNode* curr = head;
+
+//         while (curr != NULL) {
+//             ListNode* next = curr->next;  // store next
+//             curr->next = prev;            // reverse link
+//             prev = curr;                  // move prev
+//             curr = next;                  // move curr
+//         }
+
+//         return prev;  // new head
+//     }
+// };
+
+
+// recursion
+
 class Solution {
 public:
+    ListNode* rev(ListNode* head, ListNode* prev) {
+        if (head == NULL)
+            return prev;
+
+        ListNode* nextNode = head->next;
+        head->next = prev;
+        return rev(nextNode, head);
+    }
+
     ListNode* reverseList(ListNode* head) {
-        ListNode* prev = NULL;
-        ListNode* curr = head;
-
-        while (curr != NULL) {
-            ListNode* next = curr->next;  // store next
-            curr->next = prev;            // reverse link
-            prev = curr;                  // move prev
-            curr = next;                  // move curr
-        }
-
-        return prev;  // new head
+        return rev(head, NULL);
     }
 };
+
+
