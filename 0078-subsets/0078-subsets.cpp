ada48f81cac324cd7ec0a0d2a2153f44446fb9de
ayhm23 <145286298+ayhm23@users.noreply.github.com>
2026-01-21 14:53:17 +0530

    Time: 4 ms (10.13%), Space: 12.7 MB (25.87%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-    void sub(vector<vector<int>>& ans, vector<int>& nums,
-             vector<int>& curr, int i) {
-
-        if (i == nums.size()) {
+    void sub(vector<vector<int>> &ans, vector<int>& nums, int i, vector<int> &curr){
+        if(i == nums.size()){
             ans.push_back(curr);
             return;
         }
 
-        // Exclude nums[i]
-        sub(ans, nums, curr, i + 1);
-
-        // Include nums[i]
-        curr.push_back(nums[i]);
-        sub(ans, nums, curr, i + 1);
-        curr.pop_back(); // backtracking
+        int temp = nums[i];
+        sub(ans, nums, i+1, curr);
+        curr.push_back(temp);
+        sub(ans, nums, i+1, curr);
+        curr.pop_back();
     }
-
+    
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> curr;
-        sub(ans, nums, curr, 0);
+        sub(ans, nums, 0, curr);
         return ans;
     }
 };
