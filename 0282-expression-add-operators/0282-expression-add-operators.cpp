d03d113391ac12a487c891ad9576fbf996845a4e
ayhm23 <145286298+ayhm23@users.noreply.github.com>
2026-01-24 22:29:00 +0530

    Time: 387 ms (76.99%), Space: 93.3 MB (53.13%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.cpp
+++ 0282-expression-add-operators/0282-expression-add-operators.cpp
@@ -1,37 +1,48 @@
 class Solution {
 public:
-    void dfs(vector<string>& ans, string& num, int target,
-             int idx, long long eval, long long prev, string expr) {
+    void check(vector<string>& ans, string& num, int target,
+               int i, long long curr, long long prev, string currStr) {
 
-        if (idx == num.size()) {
-            if (eval == target) ans.push_back(expr);
+        if (i == num.length()) {
+            if (curr == target)
+                ans.push_back(currStr);
             return;
         }
 
-        for (int i = idx; i < num.size(); i++) {
+        for (int j = i; j < num.length(); j++) {
+
             // avoid numbers with leading zero
-            if (i > idx && num[idx] == '0') break;
+            if (j > i && num[i] == '0') break;
 
-            string part = num.substr(idx, i - idx + 1);
-            long long curr = stoll(part);
+            string part = num.substr(i, j - i + 1);
+            long long val = stoll(part);
 
-            if (idx == 0) {
-                // first number (no operator)
-                dfs(ans, num, target, i + 1, curr, curr, part);
+            if (i == 0) {
+                // first number (no operator before it)
+                check(ans, num, target, j + 1, val, val, part);
             } else {
-                dfs(ans, num, target, i + 1, eval + curr, curr, expr + "+" + part);
-                dfs(ans, num, target, i + 1, eval - curr, -curr, expr + "-" + part);
-                dfs(ans, num, target, i + 1,
-                    eval - prev + prev * curr,
-                    prev * curr,
-                    expr + "*" + part);
+                // +
+                check(ans, num, target, j + 1,
+                      curr + val, val,
+                      currStr + "+" + part);
+
+                // -
+                check(ans, num, target, j + 1,
+                      curr - val, -val,
+                      currStr + "-" + part);
+
+                // *
+                check(ans, num, target, j + 1,
+                      curr - prev + prev * val,
+                      prev * val,
+                      currStr + "*" + part);
             }
         }
     }
 
     vector<string> addOperators(string num, int target) {
         vector<string> ans;
-        dfs(ans, num, target, 0, 0, 0, "");
+        check(ans, num, target, 0, 0, 0, "");
         return ans;
     }
 };
