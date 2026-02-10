3dfcb734ce4f6de4b12fb80959630c3bc4372af8
ayhm23 <145286298+ayhm23@users.noreply.github.com>
2026-02-10 15:15:29 +0530

    Time: 0 ms (100%), Space: 10.8 MB (98.88%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -28,31 +28,60 @@
 
 
 //Iterative 2 stack
+// class Solution {
+// public:
+    
+//     vector<int> postorderTraversal(TreeNode* root) {
+//         vector<int> ans;
+//         if(!root) return ans;
+
+//         stack<TreeNode*> st1;
+//         stack<int> st2;
+
+//         st1.push(root);
+
+//         while(!st1.empty()){
+//             TreeNode * curr = st1.top();
+//             st1.pop();
+//             st2.push(curr->val);
+
+//             if(curr->left) st1.push(curr->left);
+//             if(curr->right) st1.push(curr->right);
+//         }
+
+//         while(!st2.empty()){
+//             ans.push_back(st2.top());
+//             st2.pop();
+//         }
+//         return ans;
+//     }
+// };
+
+//iterative 1 stack
 class Solution {
 public:
-    
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> ans;
-        if(!root) return ans;
+        stack<TreeNode*> st;
+        TreeNode* curr = root;
+        TreeNode* lastVisited = nullptr;
 
-        stack<TreeNode*> st1;
-        stack<int> st2;
-
-        st1.push(root);
-
-        while(!st1.empty()){
-            TreeNode * curr = st1.top();
-            st1.pop();
-            st2.push(curr->val);
-
-            if(curr->left) st1.push(curr->left);
-            if(curr->right) st1.push(curr->right);
+        while (curr != nullptr || !st.empty()) {
+            if (curr != nullptr) {
+                st.push(curr);
+                curr = curr->left;
+            } else {
+                TreeNode* node = st.top();
+                if (node->right != nullptr && lastVisited != node->right) {
+                    curr = node->right;
+                } else {
+                    ans.push_back(node->val);
+                    lastVisited = node;
+                    st.pop();
+                }
+            }
         }
 
-        while(!st2.empty()){
-            ans.push_back(st2.top());
-            st2.pop();
-        }
         return ans;
     }
 };
