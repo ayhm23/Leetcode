dddc5f848eced5e1b5c807955d7101524907e53a
ayhm23 <145286298+ayhm23@users.noreply.github.com>
2026-03-12 14:44:59 +0530

    Time: 1 ms (34.03%), Space: 22.8 MB (69.39%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,36 +1,65 @@
+// class Solution {
+// public:
+//     int candy(vector<int>& ratings) {
+        
+//         int n = ratings.size();
+//         vector<pair<int,int>> arr;
+        
+//         for(int i = 0; i < n; i++){
+//             arr.push_back({ratings[i], i});
+//         }
+
+//         sort(arr.begin(), arr.end());
+
+//         vector<int> candies(n, 1);
+
+//         for(auto &p : arr){
+
+//             int rating = p.first;
+//             int i = p.second;
+
+//             int left = 0, right = 0;
+
+//             if(i > 0 && rating > ratings[i-1]){
+//                 left = candies[i-1];
+//             }
+
+//             if(i < n-1 && rating > ratings[i+1]){
+//                 right = candies[i+1];
+//             }
+
+//             candies[i] = max(left, right) + 1;
+//         }
+
+//         return accumulate(candies.begin(), candies.end(), 0);
+//     }
+// };
+
 class Solution {
 public:
     int candy(vector<int>& ratings) {
         
         int n = ratings.size();
-        vector<pair<int,int>> arr;
-        
-        for(int i = 0; i < n; i++){
-            arr.push_back({ratings[i], i});
-        }
-
-        sort(arr.begin(), arr.end());
-
+        if(n == 1) return 1;
+        //two pass O(n) approach
         vector<int> candies(n, 1);
 
-        for(auto &p : arr){
-
-            int rating = p.first;
-            int i = p.second;
-
-            int left = 0, right = 0;
-
-            if(i > 0 && rating > ratings[i-1]){
-                left = candies[i-1];
+        //left pass
+        for(int i = 1; i < n; i++){
+            if(ratings[i] > ratings[i-1]){
+                candies[i] = candies[i-1]+1;
             }
-
-            if(i < n-1 && rating > ratings[i+1]){
-                right = candies[i+1];
-            }
-
-            candies[i] = max(left, right) + 1;
         }
 
-        return accumulate(candies.begin(), candies.end(), 0);
+        //right pass
+
+        for(int i = n - 2; i >= 0; i--){
+            if(ratings[i] > ratings[i+1]){
+                candies[i] = max(candies[i], candies[i+1] + 1);
+            }
+        }
+
+        int count = accumulate(candies.begin(), candies.end(), 0);
+        return count;
     }
 };
