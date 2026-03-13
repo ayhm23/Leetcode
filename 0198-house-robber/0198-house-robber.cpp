41970f22576a3cff4d8b1eff5b0e6004c3b164bb
ayhm23 <145286298+ayhm23@users.noreply.github.com>
2026-03-13 18:16:05 +0530

    Time: 0 ms (100%), Space: 10 MB (97.09%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -4,14 +4,16 @@ public:
         int n = nums.size();
         if(n == 1) return nums[0];
 
-        vector<int> dp(n, -1);
+        // vector<int> dp(n, -1);
 
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+        int prev2 = nums[0];
+        int prev1 = max(nums[0], nums[1]);
 
         for(int i = 2; i < n; i++){
-            dp[i] = max(dp[i-1], nums[i]+dp[i-2]);
+            int curr = max(prev1, nums[i]+prev2);
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return dp[n-1];
+        return prev1;
     }
 };
