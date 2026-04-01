b4b8c25b30a4000d6f593a1a0ef55d8dc27198df
Archit Jaju <145286298+ayhm23@users.noreply.github.com>
2026-04-01 16:55:04 +0530

    Time: 74 ms (57.67%), Space: 75.2 MB (79.35%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,26 +1,20 @@
+#define f(i,n) for (int i = 1; i <= n; i++)
 class Solution {
 public:
-    int longestPalindromeSubseq(string s) {
-        string s1 = s, s2 = s;
-        reverse(s2.begin(), s2.end());
-        int n = s.length();
-
-        vector<int> prev(n + 1, 0);
-        vector<int> curr(n + 1, 0);
-
-
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= n; j++){
-                if(s1[i-1] == s2[j-1]){
-                    curr[j] = 1 + prev[j-1];
-                }
-                else{
-                    curr[j] = max(curr[j-1], prev[j]);
-                }
+    int longestPalindromeSubseq(string s2) {
+        string s1 =s2;
+        reverse(s2.begin(),s2.end());
+        int n = s1.size();
+        vector<int> curr(n+1 , 0), prev(n+1, 0);
+        f(i,n){
+            f(j,n){
+                if(s1[i-1] == s2[j-1])curr[j] = 1 + prev[j-1];
+                else curr[j] = max(prev[j] , curr[j-1]);
             }
             prev = curr;
-            fill(curr.begin(), curr.end(), 0);
+            curr = vector<int>(n+1, 0);
         }
         return prev[n];
     }
 };
+// auto init = atexit( []() { ofstream( "display_runtime.txt" ) << "0"; } );
