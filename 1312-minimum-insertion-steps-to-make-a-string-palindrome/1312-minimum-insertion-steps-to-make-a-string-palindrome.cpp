2a66c59c696feef7c5e1f1258d275706d8d97709
Archit Jaju <145286298+ayhm23@users.noreply.github.com>
2026-04-01 17:00:31 +0530

    Time: 24 ms (74.88%), Space: 9.9 MB (88.25%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minInsertions(string s) {
+        string s1 = s, s2 = s;
+        reverse(s2.begin(), s2.end());
+        int n = s.length();
+
+        vector<int> prev(n + 1, 0);
+        vector<int> curr(n + 1, 0);
+
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                if(s1[i-1] == s2[j-1]){
+                    curr[j] = 1 + prev[j-1];
+                }
+                else{
+                    curr[j] = max(curr[j-1], prev[j]);
+                }
+            }
+            prev = curr;
+            fill(curr.begin(), curr.end(), 0);
+        }
+        return n-prev[n];
+    }
+};
