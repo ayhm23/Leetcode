65d71904dd5f00ddfd93992d87d9798e570aee4f
Archit Jaju <145286298+ayhm23@users.noreply.github.com>
2026-04-03 14:56:34 +0530

    Time: 3 ms (80.59%), Space: 17.8 MB (50.28%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -4,19 +4,21 @@ public:
         int n = prices.size();
         //0 and n+1th useslestt states for transaction
         vector<vector<int>> dp(n+1, vector<int>(2*(k+1), 0));
-
+        vector<int> curr (2*(k+1), 0);
+        vector<int> next (2*(k+1), 0);
         for(int i = n-1; i >= 0; i--){
             for(int trans = 1; trans <= 2*k; trans++){
                 int p = prices[i];
                 if(trans%2){
                     //buy
-                    dp[i][trans] = max(-p + dp[i+1][trans+1], dp[i+1][trans]);
+                    curr[trans] = max(-p +next[trans+1], next[trans]);
                 }
                 else{
-                    dp[i][trans] = max(p + dp[i+1][trans+1], dp[i+1][trans]);
+                    curr[trans] = max(p + next[trans+1], next[trans]);
                 }
             }
+            next = curr;
         }
-        return dp[0][1];
+        return next[1];
     }
 };
