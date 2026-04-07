099e4729c86a3a2ac902db2d739c4cbbc40444a8
Archit Jaju <145286298+ayhm23@users.noreply.github.com>
2026-04-07 14:56:40 +0530

    Time: 97 ms (70.72%), Space: 13.7 MB (88.41%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,26 +1,49 @@
 class Solution {
 public:
-    int DP(vector<int>& nums, int i, int j, vector<vector<int>> &dp){
-        if(i > j) return 0;
+    // int DP(vector<int>& nums, int i, int j, vector<vector<int>> &dp){
+    //     if(i > j) return 0;
         
-        if(dp[i][j] != -1) return dp[i][j];
+    //     if(dp[i][j] != -1) return dp[i][j];
         
-        int maxi = 0;
-        for(int k = i; k <= j; k++){
-            int ops = nums[i-1]*nums[k]*nums[j+1];
-            int rest = DP(nums, i, k-1, dp) + DP(nums, k+1, j, dp);
+    //     int maxi = 0;
+    //     for(int k = i; k <= j; k++){
+    //         int ops = nums[i-1]*nums[k]*nums[j+1];
+    //         int rest = DP(nums, i, k-1, dp) + DP(nums, k+1, j, dp);
             
 
-            maxi = max(rest + ops , maxi);
-        }
+    //         maxi = max(rest + ops , maxi);
+    //     }
         
-        return dp[i][j] = maxi;
-    }
+    //     return dp[i][j] = maxi;
+    // }
+
+    // int maxCoins(vector<int>& nums) {
+    //     int n = nums.size();
+    //     vector<vector<int>> dp(n+2, vector<int>(n+2, -1));
+    //     nums.insert(nums.begin(), 1); nums.push_back(1);
+    //     return DP(nums, 1, n, dp);
+    // }
 
     int maxCoins(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n+2, vector<int>(n+2, -1));
+        vector<vector<int>> dp(n+2, vector<int>(n+2, 0));
         nums.insert(nums.begin(), 1); nums.push_back(1);
-        return DP(nums, 1, n, dp);
+
+        for(int len = 1; len <= n; len++){
+            for(int i = 1; i <= n - len + 1; i++){
+                
+                int j = i + len - 1;
+                int maxi = 0;
+                //last ballon to pop
+                for(int k = i; k <= j; k++){
+                    int coins = nums[i-1]*nums[k]*nums[j+1];
+                    int rest = dp[i][k-1] + dp[k+1][j];
+
+                    maxi = max(maxi, rest+coins);
+                }
+                dp[i][j] = maxi;
+            }
+        }
+        return dp[1][n];
     }
 };
