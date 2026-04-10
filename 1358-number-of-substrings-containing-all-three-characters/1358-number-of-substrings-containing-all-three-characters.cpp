bef42489b1bd63a574656f1ca948cf2993235639
Archit Jaju <145286298+ayhm23@users.noreply.github.com>
2026-04-10 14:33:25 +0530

    Time: 11 ms (43.24%), Space: 11 MB (56.25%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,24 +1,43 @@
+// class Solution {
+// public:
+//     int atmost(string s, int k){
+//         unordered_map<char, int> mp;
+//         int l = 0, ans = 0;
+
+//         for(int r = 0; r < s.length(); r++){
+//             mp[s[r]]++;
+
+//             while(mp.size() > k){
+//                 mp[s[l]]--;
+//                 if(mp[s[l]] == 0){
+//                     mp.erase(s[l]);
+//                 }
+//                 l++;
+//             }
+//             ans += r - l + 1; 
+//         }
+//         return ans;
+//     }
+//     int numberOfSubstrings(string s) {
+//         return atmost(s, 3) - atmost(s, 2);
+//     }
+// };
+
 class Solution {
 public:
-    int atmost(string s, int k){
-        unordered_map<char, int> mp;
-        int l = 0, ans = 0;
-
-        for(int r = 0; r < s.length(); r++){
-            mp[s[r]]++;
-
-            while(mp.size() > k){
-                mp[s[l]]--;
-                if(mp[s[l]] == 0){
-                    mp.erase(s[l]);
-                }
-                l++;
-            }
-            ans += r - l + 1; 
-        }
-        return ans;
-    }
     int numberOfSubstrings(string s) {
-        return atmost(s, 3) - atmost(s, 2);
+        vector<int> last(3, -1);
+        int ans = 0;
+
+        for(int i = 0; i < s.size(); i++){
+            last[s[i] - 'a'] = i;
+
+            int minIdx = min({last[0], last[1], last[2]});
+            if(minIdx != -1){
+                ans += minIdx + 1;
+            }
+        }
+
+        return ans;
     }
 };
