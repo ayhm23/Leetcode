63a0ffd67ce01ae78e6f025d0653fe4d5b60acf6
Archit Jaju <145286298+ayhm23@users.noreply.github.com>
2026-04-19 09:58:33 +0530

    Time: 4 ms (12.34%), Space: 23.1 MB (68.24%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.cpp
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.cpp
@@ -1,18 +1,43 @@
+// class Solution {
+// public:
+//     int maxFrequencyElements(vector<int>& nums) {
+//         unordered_map<int, int> mp;
+//         int currE = nums[0], maxF = 0;
+//         for(int i = 0; i < nums.size(); i++){
+//             mp[nums[i]]++;
+//             maxF = max(maxF, mp[nums[i]]);
+//         }
+//         int ans = 0;
+//         for(auto &[num, count] : mp){
+//             if(count == maxF){
+//                 ans += maxF;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     int maxFrequencyElements(vector<int>& nums) {
         unordered_map<int, int> mp;
-        int currE = nums[0], maxF = 0;
-        for(int i = 0; i < nums.size(); i++){
-            mp[nums[i]]++;
-            maxF = max(maxF, mp[nums[i]]);
-        }
-        int ans = 0;
-        for(auto &[num, count] : mp){
-            if(count == maxF){
-                ans += maxF;
+
+        int maxF = 0;
+        int countMaxF = 0;
+
+        for(int num : nums){
+            mp[num]++;
+            int freq = mp[num];
+
+            if(freq > maxF){
+                maxF = freq;
+                countMaxF = 1;
+            }
+            else if(freq == maxF){
+                countMaxF++;
             }
         }
-        return ans;
+
+        return maxF * countMaxF;
     }
 };
