5318df8ef2aa7e6424c864ade1c37b130fd55670
Archit Jaju <145286298+ayhm23@users.noreply.github.com>
2026-06-03 21:01:50 +0530

    Time: 0 ms (100%), Space: 14.9 MB (18.55%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // approach 1.. two nested look.. i and i to n -1 O(n^2) O(1)
-        // approach 2.. hashing (index, value)initially.. look for target - nums[i] in hashmap...O(N) and O(N)
-        // approach 3.. vector<pair(value,index)> sort by values.. for ith value use binary search for target - nums[i] O(NlogN) O(N)
-
-        unordered_map<int,int> map;
-        
+        unordered_map<int, int>mp;
+        vector<int> ans;
         for(int i = 0; i < nums.size(); i++){
-            int x = target - nums[i];
-
-            if(map.find(x) != map.end()){
-                return {map[x], i};
+            if(mp.find(target - nums[i]) != mp.end()){
+                ans = {mp[target - nums[i]], i}; break;
             }
-            map[nums[i]] = i;
+            mp[nums[i]] = i;
         }
-    return {};
+        return ans;
     }
 };
