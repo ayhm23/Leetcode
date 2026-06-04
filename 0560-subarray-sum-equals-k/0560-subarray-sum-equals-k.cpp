02b9f73017e9f9be075db4424fc4b5ffe9ca5d04
Archit Jaju <145286298+ayhm23@users.noreply.github.com>
2026-06-04 16:59:38 +0530

    Time: 39 ms (71.04%), Space: 45.4 MB (71.69%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        // sol1 : make all subarrays O(N^2) O(N^2)
-        // sol2 : prefix sum O(N^2) O(N)
-        // sol3 : hashmap to keep count of exisitng sum
-
-        unordered_map <int, int> map; 
-        map[0] = 1;
-        int n = nums.size();
+        unordered_map<int, int> mp;
         int sum = 0, count = 0;
-        for(int i = 0; i < n; i++){
-            sum += nums[i];
-            int x = sum - k;
-            if(map.find(x) != map.end()){
-                count += map[x];
-            }
-            map[sum]++;
+        mp[0] = 1;
+
+        for(int x : nums){
+            sum += x;
+            if(mp.count(sum - k)) count+=mp[sum-k];
+            mp[sum]++;
         }
-    return count;
+        return count;   
     }
 };
