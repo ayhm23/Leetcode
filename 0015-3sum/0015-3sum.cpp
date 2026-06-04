3ef0688bbfbe22269b0ee9ddf04b6806d44d5907
Archit Jaju <145286298+ayhm23@users.noreply.github.com>
2026-06-04 17:28:47 +0530

    Time: 47 ms (65.48%), Space: 29.1 MB (72.06%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -2,26 +2,26 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> ans;
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end()); int n = nums.size();
+        for(int i = 0; i < n-2; i++){
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
 
-        for (int i = 0; i < n; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            int l = i + 1, r = n - 1;
-
-            while (l < r) {
-                int sum = nums[i] + nums[l] + nums[r];
-
-                if (sum == 0) {
-                    ans.push_back({nums[i], nums[l], nums[r]});
+            int curr = nums[i];//find two nums with sum -curr //already sorted only need 2 poninter l and r
+            int l = i+1, r = n-1;
+            while(l < r){
+                int s = nums[l] + nums[r] + curr;
+                if(s == 0){
+                    ans.push_back({curr, nums[l], nums[r]});
+                    while(l < r && nums[l] == nums[l+1])l++;
+                    while(l < r && nums[r] == nums[r-1])r--;
                     l++; r--;
-
-                    while (l < r && nums[l] == nums[l - 1]) l++;
-                    while (l < r && nums[r] == nums[r + 1]) r--;
                 }
-                else if (sum < 0) l++;
-                else r--;
+                else if(s < 0){
+                    l++;
+                }
+                else{
+                    r--;
+                }
             }
         }
         return ans;
