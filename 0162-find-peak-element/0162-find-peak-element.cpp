6b05ba8c40e2059f26911f2c44336b2db2a95794
Archit Jaju <145286298+ayhm23@users.noreply.github.com>
2026-06-06 15:37:57 +0530

    Time: 0 ms (100%), Space: 12.6 MB (70.81%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,48 +1,17 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int low = 0, high = nums.size() - 1;
-        int n = nums.size();
-        int ans = -1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            // Case: mid in between
-            if (mid > 0 && mid < n - 1) {
-                if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
-                    ans = mid;
-                    break;
-                }
-                else if (nums[mid] < nums[mid + 1]) {
-                    low = mid + 1;
-                }
-                else {
-                    high = mid - 1;
-                }
+        //idea is.. if mid + 1 > mid..then peak is to the right/..else <= mid can be peak
+        int lo = 0, hi = nums.size()-1;
+        while(lo < hi){
+            int mid = lo + (hi - lo)/2;
+            if(nums[mid] < nums[mid+1]){
+                lo = mid + 1;
             }
-            // Case: mid == 0 (only check right neighbor to avoid overflow)
-            else if (mid == 0) {
-                if (n == 1 || nums[mid] > nums[mid + 1]) {
-                    ans = mid;
-                    break;
-                }
-                else {
-                    low = mid + 1;
-                }
-            }
-            // Case: mid == n-1 (only check left neighbor)
-            else if (mid == n - 1) {
-                if (nums[mid] > nums[mid - 1]) {
-                    ans = mid;
-                    break;
-                }
-                else {
-                    high = mid - 1;
-                }
+            else{
+                hi = mid;
             }
         }
-
-        return ans;
+        return hi;
     }
 };
