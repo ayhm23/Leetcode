6f175116d0a1c3f01f6a6b54b35b56ccf2a36691
Archit Jaju <145286298+ayhm23@users.noreply.github.com>
2026-06-06 18:21:51 +0530

    Time: 0 ms (100%), Space: 26 MB (92.87%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -3,35 +3,18 @@ public:
     int singleNonDuplicate(vector<int>& nums) {
         int lo = 0, hi = nums.size() - 1;
 
-        while (lo <= hi) {
+        while (lo < hi) {
             int mid = lo + (hi - lo) / 2;
 
-            // single element found
-            if ((mid == 0 || nums[mid] != nums[mid - 1]) &&
-                (mid == nums.size() - 1 || nums[mid] != nums[mid + 1]))
-                return nums[mid];
+            if (mid % 2 == 1)
+                mid--;  // make mid even
 
-            // paired with right
-            if (mid < nums.size() - 1 && nums[mid] == nums[mid + 1]) {
-                int len = hi - mid;   // elements from mid+1 to hi
-
-                if (len % 2 == 0)
-                    lo = mid + 2;
-                else
-                    hi = mid - 1;
-            }
-
-            // paired with left
-            else {
-                int len = mid - lo;   // elements from lo to mid-1
-
-                if (len % 2 == 0)
-                    hi = mid - 2;
-                else
-                    lo = mid + 1;
-            }
+            if (nums[mid] == nums[mid + 1])
+                lo = mid + 2;
+            else
+                hi = mid;
         }
 
-        return -1;
+        return nums[lo];
     }
 };
