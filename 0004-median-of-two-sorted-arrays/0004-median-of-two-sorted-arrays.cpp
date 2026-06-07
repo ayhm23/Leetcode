04cf475b86a7ec116500f1be7bc20b177caf3893
Archit Jaju <145286298+ayhm23@users.noreply.github.com>
2026-06-07 16:59:08 +0530

    Time: 4 ms (19.12%), Space: 95.1 MB (79.19%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,79 +1,30 @@
-// class Solution {
-// public:
-//     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-//         // ensure nums1 is the smaller array
-//         if (nums1.size() > nums2.size()) 
-//             return findMedianSortedArrays(nums2, nums1);
-
-//         int n1 = nums1.size(), n2 = nums2.size();
-//         int total = n1 + n2;
-//         int half = (total + 1) / 2;
-
-//         int lo = 0, hi = n1;
-
-//         while (lo <= hi) {
-//             int cut1 = (lo + hi) / 2;       // partition in nums1
-//             int cut2 = half - cut1;         // partition in nums2
-
-//             int l1 = (cut1 == 0) ? INT_MIN : nums1[cut1 - 1];
-//             int l2 = (cut2 == 0) ? INT_MIN : nums2[cut2 - 1];
-//             int r1 = (cut1 == n1) ? INT_MAX : nums1[cut1];
-//             int r2 = (cut2 == n2) ? INT_MAX : nums2[cut2];
-
-//             if (l1 <= r2 && l2 <= r1) {
-//                 if (total % 2 == 0) {
-//                     return (max(l1, l2) + min(r1, r2)) / 2.0;
-//                 } else {
-//                     return max(l1, l2);
-//                 }
-//             }
-//             else if (l1 > r2) {
-//                 hi = cut1 - 1;
-//             }
-//             else {
-//                 lo = cut1 + 1;
-//             }
-//         }
-//         return 0.0; // never reached
-//     }
-// };
-
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if (nums1.size() > nums2.size()) 
-            return findMedianSortedArrays(nums2, nums1);
-
         int n1 = nums1.size(), n2 = nums2.size();
-        int lo = 0, hi = n1;
-        int totalLeft = (n1 + n2 + 1) / 2; // total elements on left side
+        if (n1 > n2)
+            return findMedianSortedArrays(nums2, nums1);
+        int median = (n1 + n2 + 1)/2; 
+        int lo1 = 0, lo2 = 0, hi1 = n1, hi2 = n2;
 
-        while (lo <= hi) {
-            int cut1 = (lo + hi) / 2;
-            int cut2 = totalLeft - cut1;
+        while(lo1 <= hi1 && lo2 <= hi2){
+            int cut1 = lo1 + (hi1-lo1)/2, cut2 = median - cut1;
 
-            int l1 = (cut1 == 0) ? INT_MIN : nums1[cut1 - 1];
-            int l2 = (cut2 == 0) ? INT_MIN : nums2[cut2 - 1];
-            int r1 = (cut1 == n1) ? INT_MAX : nums1[cut1];
-            int r2 = (cut2 == n2) ? INT_MAX : nums2[cut2];
+            int l1 = cut1 != 0 ? nums1[cut1-1] : INT_MIN;
+            int l2 = cut2 != 0 ? nums2[cut2-1] : INT_MIN;
+            int r1 = cut1 != n1 ? nums1[cut1] : INT_MAX;
+            int r2 = cut2 != n2 ? nums2[cut2] : INT_MAX; 
 
-            if (l1 <= r2 && l2 <= r1) {
-                if ((n1 + n2) % 2 == 0) {
-                    return (max(l1, l2) + min(r1, r2)) / 2.0;
-                } else {
+            if(l1 <= r2 && l2 <= r1){
+                if ((n1 + n2) % 2) // odd
                     return max(l1, l2);
-                }
-            } 
-            else if (l1 > r2) {
-                hi = cut1 - 1;
-            } 
-            else {
-                lo = cut1 + 1;
+                return (max(l1, l2) + min(r1, r2))/2.0;
+            }
+            else{
+                if(l1 > r2) hi1 = cut1-1;
+                else if(l2 > r1) lo1 = cut1 + 1;
             }
         }
-        return 0.0; // should never reach here
+        return 0.0;
     }
 };
