0043c102a3fe52702afa4c494def3c258c41b12d
Archit Jaju <145286298+ayhm23@users.noreply.github.com>
2026-06-08 10:23:43 +0530

    Time: 3 ms (36.09%), Space: 11.1 MB (27.64%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -4,15 +4,16 @@ public:
         istringstream iss(s);
         vector<string> words;
         string word;
-        while(iss >> word){
+
+        while (iss >> word)
             words.push_back(word);
+
+        string ans;
+        for (int i = words.size() - 1; i >= 0; i--) {
+            ans += words[i];
+            if (i) ans += " ";
         }
-        reverse(words.begin(), words.end());
-        word = "";
-        for(int i = 0; i < words.size(); i++){
-            word += words[i]; 
-            if(i + 1 != words.size()) word += " ";
-        }
-        return word;
+
+        return ans;
     }
 };
