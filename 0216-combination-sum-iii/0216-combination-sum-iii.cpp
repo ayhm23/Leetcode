a612229ffb29d2a3c9fe3343aef7c0e2ab57e8f1
Archit Jaju <145286298+ayhm23@users.noreply.github.com>
2026-06-14 00:27:37 +0530

    Time: 2 ms (20.85%), Space: 9.3 MB (17.62%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    void sums(vector<vector<int>> &ans, vector<int>& nums, int idx, 
-    int k, int n, vector<int> & curr, int sum){
-        if (curr.size() == k) {
-            if (sum == n)
-                ans.push_back(curr);
+    vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    vector<vector<int>> ans;
+
+    void count(int k, int n, vector<int> &curr, int ind){
+        if(curr.size() == k){
+            if(n == 0) ans.push_back(curr); 
             return;
         }
+        
+        if(n < 0 || ind == 9) return;
 
-        if (idx == nums.size() || sum > n) return;
+        count(k, n, curr, ind + 1);
 
-
-        curr.push_back(nums[idx]);
-        sums(ans, nums, idx+1, k, n, curr, sum+nums[idx]);
+        curr.push_back(nums[ind]);
+        count(k, n - nums[ind], curr, ind + 1);
         curr.pop_back();
-
-        sums(ans, nums, idx+1, k, n, curr, sum);
     }
-
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<int> nums = {1,2,3,4,5,6,7,8,9};
-        vector<vector<int>> ans;
         vector<int> curr;
-
-        sums(ans, nums, 0, k, n, curr, 0);
+        count(k, n, curr, 0);
         return ans;
     }
 };
