0673cc80a04587d576108f0c46ea62dfb565a34b
Archit Jaju <145286298+ayhm23@users.noreply.github.com>
2026-06-13 16:37:37 +0530

    Time: 0 ms (100%), Space: 8.7 MB (42.28%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        long long N = n;          // handle INT_MIN safely
-
-        if (N < 0) {
-            x = 1 / x;
-            N = -N;
+    double Pow(double x, long long n){
+        if(x == 1 || n == 0) return 1;
+        if(x == 0) return 0;
+        if(n%2){
+            //odd
+            return x*Pow(x*x, n/2);
         }
-
-        if (N == 0) return 1;
-
-        double half = myPow(x, N / 2);
-
-        if (N % 2 == 0)
-            return half * half;
-        else
-            return half * half * x;
+        else{
+            //even
+            return Pow(x*x, n/2);
+        }
+    } 
+    double myPow(double x, int n) {
+        long long N = n;
+        if(n < 0) {
+            N = -1*N; x = 1/x;
+        }
+        return Pow(x, N);
+        
     }
 };
