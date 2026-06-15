7ecf9bf2d10988fc298e2bd23341183f6957c33a
Archit Jaju <145286298+ayhm23@users.noreply.github.com>
2026-06-15 19:48:51 +0530

    Time: 44 ms (24.08%), Space: 151 MB (22.25%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,29 +1,60 @@
 class MinStack {
 public:
-    stack<int> st;
-    stack<int> minSt;
-
-    MinStack() {}
-
-    void push(int val) {
-        st.push(val);
-        if(minSt.empty() || val <= minSt.top()) {
-            minSt.push(val);
+    int MinVal;
+    stack<long long> st;
+    //encode if new min value storeed
+    MinStack() {
+        
+    }
+    
+    void push(int value) {
+        if(st.empty()){
+            st.push(value); MinVal = value;
+            return;
+        }
+        else if(value >= MinVal){
+            st.push(value);
+        }
+        else{
+            //smaller than MinVal
+            //we need to store the current MINVAL..encode it
+            st.push(2LL*value - MinVal);
+            MinVal = value;
         }
     }
-
+    
     void pop() {
-        if(st.top() == minSt.top()) {
-            minSt.pop();
+        if(st.empty()) return;
+        long long val = st.top(); st.pop();
+        if(val < MinVal){
+            MinVal = 2LL*MinVal - val;
         }
-        st.pop();
     }
-
+    
     int top() {
-        return st.top();
-    }
+        if(st.empty()) return -1;
 
+        long long val = st.top();
+        if(val < MinVal){
+            return MinVal;
+        }
+        else{
+            return (int)val;
+        }
+    }
+    
     int getMin() {
-        return minSt.top();
+        if(st.empty()) return -1;
+
+        return MinVal;
     }
 };
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(value);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
