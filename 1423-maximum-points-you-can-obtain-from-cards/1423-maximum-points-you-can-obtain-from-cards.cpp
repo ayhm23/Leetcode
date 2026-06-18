35e765a31bd486aa60264bd04f5f983272fbb8cc
Archit Jaju <145286298+ayhm23@users.noreply.github.com>
2026-06-18 23:59:35 +0530

    Time: 0 ms (100%), Space: 46.4 MB (32.97%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     int maxScore(vector<int>& cardPoints, int k) {
-        //sum of first k element...then we removve the right most from start in winodw and add from back
-        int sum = accumulate(cardPoints.begin(), cardPoints.begin() + k, 0);
         int n = cardPoints.size();
-        if(n == k) return sum;
+        int curr, maxx;
+        curr = maxx = accumulate(cardPoints.begin(), cardPoints.begin() + k, 0);
 
-        int ans = sum;
-
-        int left = k-1, right = n-1;
-   
-        while(left >= 0){
-            sum = sum - cardPoints[left] + cardPoints[right];
-            ans = max(sum, ans);
-            left--; right--;
+        for(int i = 0; i < k; i++){
+            curr = curr - cardPoints[k-1-i] + cardPoints[n-1-i];
+            maxx = max(maxx, curr);
         }
-        return ans;
+        return maxx;
     }
 };
