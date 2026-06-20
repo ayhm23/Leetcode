749ccd5a00f58f590a068f731de404b2bae6a282
Archit Jaju <145286298+ayhm23@users.noreply.github.com>
2026-06-20 18:10:25 +0530

    Time: 45 ms (16.56%), Space: 72.5 MB (15.48%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,33 +1,11 @@
 class Solution {
 public:
-    void heapify(vector<int>& nums, int n, int i){
-        int largest = i;
-
-        int r = 2*i + 2, l = 2*i + 1;
-        if(r < n && nums[r] > nums[largest]) largest = r;
-        if(l < n && nums[l] > nums[largest]) largest = l;
-
-        if(largest != i){
-            swap(nums[largest], nums[i]);
-            heapify(nums, n, largest);
-        }
-    }
-
-    void makeMaxHeap(vector<int>& nums, int n){
-        for(int i = (n-1)/2; i >= 0; i--){
-            heapify(nums, n, i); // ✅ fixed
-        }
-    }
-
     int findKthLargest(vector<int>& nums, int k) {
-        int n = nums.size();
-        makeMaxHeap(nums, n);
-
-        for(int i = 1; i < k; i++){
-            swap(nums[0], nums[nums.size()-1]); // ✅ fixed
-            nums.pop_back();
-            heapify(nums, nums.size(), 0);
+        priority_queue<int, vector<int>, greater<int>> minH;
+        for(int i : nums){
+            minH.push(i);
+            if(minH.size() > k) minH.pop();
         }
-        return nums[0];
+        return minH.top();
     }
 };
