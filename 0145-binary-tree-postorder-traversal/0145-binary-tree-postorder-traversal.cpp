8bae71c108809c356eec148b0e26c9daf85fc2a2
Archit Jaju <145286298+ayhm23@users.noreply.github.com>
2026-07-06 18:02:12 +0530

    Time: 0 ms (100%), Space: 10.9 MB (93.16%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -9,79 +9,46 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-// class Solution {
-// public:
-//     void postOrder(TreeNode* root, vector<int> & ans){
-//         if(!root) return;
-
-//         postOrder(root->left, ans);
-//         postOrder(root->right, ans);
-//         ans.push_back(root->val);
-//     }
-//     vector<int> postorderTraversal(TreeNode* root) {
-//         vector<int> ans;
-//         postOrder(root, ans);
-//         return ans;
-//     }
-// };
-
-
-//Iterative 2 stack
-// class Solution {
-// public:
-    
-//     vector<int> postorderTraversal(TreeNode* root) {
-//         vector<int> ans;
-//         if(!root) return ans;
-
-//         stack<TreeNode*> st1;
-//         stack<int> st2;
-
-//         st1.push(root);
-
-//         while(!st1.empty()){
-//             TreeNode * curr = st1.top();
-//             st1.pop();
-//             st2.push(curr->val);
-
-//             if(curr->left) st1.push(curr->left);
-//             if(curr->right) st1.push(curr->right);
-//         }
-
-//         while(!st2.empty()){
-//             ans.push_back(st2.top());
-//             st2.pop();
-//         }
-//         return ans;
-//     }
-// };
-
-//iterative 1 stack
 class Solution {
 public:
-    vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        stack<TreeNode*> st;
-        TreeNode* curr = root;
-        TreeNode* lastVisited = nullptr;
+    // vector<int> postorderTraversal(TreeNode* root) {
+    //     vector<int> ans; if(!root) return ans;
+    //     stack<TreeNode*> st1, st2;
+    //     st1.push(root);
 
-        while (curr != nullptr || !st.empty()) {
-            if (curr != nullptr) {
-                st.push(curr);
-                curr = curr->left;
-            } else {
-                TreeNode* node = st.top();
-                if (node->right != nullptr && lastVisited != node->right) {
-                    curr = node->right;
-                } else {
-                    ans.push_back(node->val);
-                    lastVisited = node;
-                    st.pop();
+    //     while(!st1.empty()){
+    //         auto it = st1.top(); st1.pop();
+    //         st2.push(it);
+    //         if(it->left) st1.push(it->left);
+    //         if(it->right) st1.push(it->right);
+    //     }
+
+    //     while(!st2.empty()){
+    //         ans.push_back(st2.top()->val);
+    //         st2.pop();
+    //     }
+    //     return ans;
+    // }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans; if(!root) return ans;
+        stack<TreeNode*> st; 
+        TreeNode* cur = root, *last = NULL;
+
+        while(cur || !st.empty()){
+            if(cur){
+                st.push(cur); cur = cur->left;
+            }
+            else{
+                TreeNode * peek = st.top();
+                if(peek->right && last != peek->right){
+                    cur = peek->right;
+                }
+                else{
+                    ans.push_back(peek->val);
+                    last = peek; st.pop();
                 }
             }
         }
-
         return ans;
     }
 };
