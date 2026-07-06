f8bd8e2dedbf4ecc9b5fecb638a5d738849680d7
Archit Jaju <145286298+ayhm23@users.noreply.github.com>
2026-07-06 16:37:10 +0530

    Time: 0 ms (100%), Space: 10.9 MB (66.06%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -1,49 +1,27 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    // void inorder(TreeNode* root, vector<int> & ans){
-    //     if(!root) return;
-
-    //     inorder(root->left, ans);
-    //     ans.push_back(root->val);
-    //     inorder(root->right, ans);
-    // }
-    // vector<int> inorderTraversal(TreeNode* root) {
-    //     vector<int> ans;
-    //     inorder(root, ans);
-    //     return ans;
-    // }
-
-
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> ans;
         stack<TreeNode*> st;
-        TreeNode* curr = root;
+        TreeNode* node = root;
 
-        while (curr != nullptr || !st.empty()) {
-            // go to the leftmost node
-            while (curr != nullptr) {
-                st.push(curr);
-                curr = curr->left;
+        vector<int> ans;
+
+        while (node || !st.empty()) {
+
+            // go left as far as possible
+            while (node) {
+                st.push(node);
+                node = node->left;
             }
 
-            // process node
-            curr = st.top();
+            // visit node
+            node = st.top();
             st.pop();
-            ans.push_back(curr->val);
+
+            ans.push_back(node->val);
 
             // move to right subtree
-            curr = curr->right;
+            node = node->right;
         }
 
         return ans;
