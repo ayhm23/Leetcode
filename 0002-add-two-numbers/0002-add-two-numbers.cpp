82b3e73cee9e7c2c5c9fc08d3802ea64c1c9cdd0
Archit Jaju <145286298+ayhm23@users.noreply.github.com>
2026-07-30 14:48:43 +0530

    Time: 0 ms (100%), Space: 77.2 MB (13.14%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,27 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode dummy(0);
-        ListNode* curr = &dummy;
-        int carry = 0;
-
-        while (l1 || l2 || carry) {
-            int sum = carry;
-
-            if (l1) {
-                sum += l1->val;
-                l1 = l1->next;
-            }
-            if (l2) {
-                sum += l2->val;
-                l2 = l2->next;
-            }
-
-            curr->next = new ListNode(sum % 10);
-            carry = sum / 10;
-            curr = curr->next;
+    ListNode* add(int carry, ListNode* l1, ListNode* l2){
+        if(!l1 && !l2 && !carry){
+            return nullptr;
         }
-
-        return dummy.next;
+        int curr = carry;
+        curr += (l1 ? l1->val : 0);
+        curr += (l2 ? l2->val : 0);
+        ListNode* ans = new ListNode(curr % 10);
+        ans->next = add(curr/10, l1 ? l1->next : nullptr, l2 ? l2->next : nullptr);
+        return ans;
+    }
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        return add(0, l1, l2);
     }
 };
