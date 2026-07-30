e6d6ec817adc63df19fde0c7a089f1e5038c964f
Archit Jaju <145286298+ayhm23@users.noreply.github.com>
2026-07-30 14:01:44 +0530

    Time: 0 ms (100%), Space: 95.2 MB (62.05%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -2,29 +2,46 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int n1 = nums1.size(), n2 = nums2.size();
-        if (n1 > n2)
+
+        if(n1 > n2)
             return findMedianSortedArrays(nums2, nums1);
-        int median = (n1 + n2 + 1)/2; 
-        int lo1 = 0, lo2 = 0, hi1 = n1, hi2 = n2;
 
-        while(lo1 <= hi1 && lo2 <= hi2){
-            int cut1 = lo1 + (hi1-lo1)/2, cut2 = median - cut1;
+        int totalLeft = (n1 + n2 + 1) / 2;
 
-            int l1 = cut1 != 0 ? nums1[cut1-1] : INT_MIN;
-            int l2 = cut2 != 0 ? nums2[cut2-1] : INT_MIN;
-            int r1 = cut1 != n1 ? nums1[cut1] : INT_MAX;
-            int r2 = cut2 != n2 ? nums2[cut2] : INT_MAX; 
+        int l = 0, r = n1;
 
+        while(l <= r){
+            int cut1 = l + (r-l)/2;
+            int cut2 = totalLeft - cut1;
+
+            int l1 = (cut1 == 0) ? INT_MIN : nums1[cut1-1];
+            int l2 = (cut2 == 0) ? INT_MIN : nums2[cut2-1];
+
+            int r1 = (cut1 == n1) ? INT_MAX : nums1[cut1];
+            int r2 = (cut2 == n2) ? INT_MAX : nums2[cut2];
+
+            // correct partition
             if(l1 <= r2 && l2 <= r1){
-                if ((n1 + n2) % 2) // odd
-                    return max(l1, l2);
-                return (max(l1, l2) + min(r1, r2))/2.0;
+
+                // odd length
+                if((n1+n2)%2)
+                    return max(l1,l2);
+
+                // even length
+                return (max(l1,l2) + min(r1,r2))/2.0;
             }
+
+            // too many elements from nums1
+            else if(l1 > r2){
+                r = cut1 - 1;
+            }
+
+            // too few elements from nums1
             else{
-                if(l1 > r2) hi1 = cut1-1;
-                else if(l2 > r1) lo1 = cut1 + 1;
+                l = cut1 + 1;
             }
         }
-        return 0.0;
+
+        return 0;
     }
 };
