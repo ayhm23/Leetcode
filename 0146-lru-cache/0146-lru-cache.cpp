aa5e820d39e55561a0376eda42ec890a988814f7
Archit Jaju <145286298+ayhm23@users.noreply.github.com>
2026-07-30 15:48:15 +0530

    Time: 107 ms (24.25%), Space: 182.2 MB (33.28%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,47 +1,45 @@
 class LRUCache {
-public:
-    list<pair<int,int>> dll; 
+    list<pair<int,int>> lis;
     unordered_map<int, list<pair<int,int>>::iterator> mp;
     int cap;
 
+public:
     LRUCache(int capacity) {
         cap = capacity;
     }
-
-    void makeRecentlyUsed(int key, int value) {
-        dll.push_front({key, value});
-        mp[key] = dll.begin();
-    }
-
+    
     int get(int key) {
-        if(mp.find(key) == mp.end())
-            return -1;
-
-        auto it = mp[key];
+        if(mp.count(key) == 0) return -1;
+        auto& it = mp[key];
         int val = it->second;
-
-        dll.erase(it);
-        mp.erase(key);
-
-        makeRecentlyUsed(key, val);
-
+        lis.erase(it);
+        lis.push_front({key, val});
+        mp[key] = lis.begin();
+        
         return val;
     }
-
+    
     void put(int key, int value) {
-
-        if(mp.find(key) != mp.end()) {
+        if(mp.count(key) != 0){
             auto it = mp[key];
-            dll.erase(it);
+            lis.erase(it);
+        }
+
+        lis.push_front({key, value});
+        mp[key] = lis.begin(); 
+        
+        if(lis.size() > cap){
+            auto it = lis.back();
+            int key = it.first;
+            lis.pop_back();
             mp.erase(key);
         }
-
-        if(dll.size() == cap) {
-            auto last = dll.back();
-            mp.erase(last.first);
-            dll.pop_back();
-        }
-
-        makeRecentlyUsed(key, value);
     }
 };
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
