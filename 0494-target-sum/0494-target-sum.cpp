7952c314b6d7bca33a6ff0c8a831799fd42d7ea2
Archit Jaju <145286298+ayhm23@users.noreply.github.com>
2026-08-22 23:29:58 +0530

    Time: 13 ms (52.82%), Space: 25.8 MB (27.14%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,33 +1,23 @@
 class Solution {
+    vector<vector<int>> dp;
+    int totalSum;
+
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
+        totalSum = accumulate(nums.begin(), nums.end(), 0);
+        dp = vector<vector<int>>(nums.size(), vector<int>(2 * totalSum + 1, INT_MIN));
+        return backtrack(0, 0, nums, target);
+    }
 
-        int n = nums.size();
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-
-        if (target > sum || target < -sum) return 0;
-
-        vector<vector<int>> dp(n+1, vector<int>(2*sum + 1, 0));
-
-        dp[0][sum] = 1; // sum = 0
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= 2*sum; j++) {
-
-                int currS = j - sum;
-
-                int plus = 0, minus = 0;
-
-                if (currS - nums[i-1] >= -sum)
-                    plus = dp[i-1][(currS - nums[i-1]) + sum];
-
-                if (currS + nums[i-1] <= sum)
-                    minus = dp[i-1][(currS + nums[i-1]) + sum];
-
-                dp[i][j] = plus + minus;
-            }
+    int backtrack(int i, int total, vector<int>& nums, int target) {
+        if (i == nums.size()) {
+            return total == target;
         }
-
-        return dp[n][target + sum];
+        if (dp[i][total + totalSum] != INT_MIN) {
+            return dp[i][total + totalSum];
+        }
+        dp[i][total + totalSum] = backtrack(i + 1, total + nums[i], nums, target) +
+                                  backtrack(i + 1, total - nums[i], nums, target);
+        return dp[i][total + totalSum];
     }
 };
