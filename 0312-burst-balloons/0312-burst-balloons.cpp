8482e90ebe428f6c52d2d71878b452fe28211cf8
Archit Jaju <145286298+ayhm23@users.noreply.github.com>
2026-08-24 07:04:18 +0530

    Time: 107 ms (64.64%), Space: 13.8 MB (84.12%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,49 +1,26 @@
 class Solution {
 public:
-    // int DP(vector<int>& nums, int i, int j, vector<vector<int>> &dp){
-    //     if(i > j) return 0;
-        
-    //     if(dp[i][j] != -1) return dp[i][j];
-        
-    //     int maxi = 0;
-    //     for(int k = i; k <= j; k++){
-    //         int ops = nums[i-1]*nums[k]*nums[j+1];
-    //         int rest = DP(nums, i, k-1, dp) + DP(nums, k+1, j, dp);
-            
-
-    //         maxi = max(rest + ops , maxi);
-    //     }
-        
-    //     return dp[i][j] = maxi;
-    // }
-
-    // int maxCoins(vector<int>& nums) {
-    //     int n = nums.size();
-    //     vector<vector<int>> dp(n+2, vector<int>(n+2, -1));
-    //     nums.insert(nums.begin(), 1); nums.push_back(1);
-    //     return DP(nums, 1, n, dp);
-    // }
-
     int maxCoins(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n+2, vector<int>(n+2, 0));
-        nums.insert(nums.begin(), 1); nums.push_back(1);
+        vector<int> balloons(n + 2, 1);
+        for (int i = 0; i < n; ++i) {
+            balloons[i + 1] = nums[i];
+        }
 
-        for(int len = 1; len <= n; len++){
-            for(int i = 1; i <= n - len + 1; i++){
-                
-                int j = i + len - 1;
-                int maxi = 0;
-                //last ballon to pop
-                for(int k = i; k <= j; k++){
-                    int coins = nums[i-1]*nums[k]*nums[j+1];
-                    int rest = dp[i][k-1] + dp[k+1][j];
+        vector<vector<int>> dp(n + 2, vector<int>(n + 2, 0));
 
-                    maxi = max(maxi, rest+coins);
+        // length is the interval length between left and right
+        for (int length = 2; length <= n + 1; ++length) {
+            for (int left = 0; left + length <= n + 1; ++left) {
+                int right = left + length;
+                for (int i = left + 1; i < right; ++i) {
+                    int coins = balloons[left] * balloons[i] * balloons[right];
+                    coins += dp[left][i] + dp[i][right];
+                    dp[left][right] = max(dp[left][right], coins);
                 }
-                dp[i][j] = maxi;
             }
         }
-        return dp[1][n];
+
+        return dp[0][n + 1];
     }
 };
