274b6a549f1778a39d32ab54037e1e567cbb6167
Archit Jaju <145286298+ayhm23@users.noreply.github.com>
2026-09-07 16:35:50 +0530

    Time: 95 ms (35.97%), Space: 113.8 MB (45.74%) - LeetHub

---
+++ 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    long long maxTaxiEarnings(int n, vector<vector<int>>& rides) {
+        //sort by end time;
+
+        sort(rides.begin(), rides.end(), [&](auto& a, auto& b){
+            if(a[1] >= b[1]) return false;
+            else return true;
+        });
+        vector<int> ends;
+        for(auto& it : rides){
+            ends.push_back(it[1]);
+        }
+
+        using ll = long long;
+        vector<ll> dp(n+1, 0LL); //dp[i] = till distance i whats best profit;
+        for(int i = 1; i <= n; i++){
+            dp[i] = dp[i-1];
+
+            auto idx = lower_bound(ends.begin(), ends.end(), i); //find if any rides ends at i 
+            if(idx == ends.end()) continue;
+            
+            for(int j = idx - ends.begin(); j < rides.size(); j++){
+                if(ends[j] != i) break;
+                ll rideProfit = rides[j][1] - rides[j][0] + rides[j][2];
+                ll lastP =  dp[rides[j][0]];
+                dp[i] = max(dp[i], rideProfit + lastP);
+            }
+        }
+        return dp[n];
+    }
+};
