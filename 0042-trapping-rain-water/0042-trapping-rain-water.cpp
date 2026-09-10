4d959cc3bdbcbc564df75c1a4cadf82c5a6fc629
Archit Jaju <145286298+ayhm23@users.noreply.github.com>
2026-09-10 11:03:40 +0530

    Time: 0 ms (100%), Space: 27.4 MB (16.37%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,44 +2,24 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        if(n == 0) return 0;
+        if (n <= 2) return 0;
 
-        vector<int> leftMax(n), rightMax(n);
+        vector<int> left_max(n), right_max(n);
 
-        // Build left max
-        leftMax[0] = height[0];
-        for(int i = 1; i < n; i++) {
-            leftMax[i] = max(leftMax[i-1], height[i]);
+        left_max[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            left_max[i] = max(left_max[i - 1], height[i]);
         }
 
-        // Build right max
-        rightMax[n-1] = height[n-1];
-        for(int i = n-2; i >= 0; i--) {
-            rightMax[i] = max(rightMax[i+1], height[i]);
+        right_max[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            right_max[i] = max(right_max[i + 1], height[i]);
         }
 
-        // Calculate trapped water
-        int water = 0;
-        for(int i = 0; i < n; i++) {
-            water += min(leftMax[i], rightMax[i]) - height[i];
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += (min(left_max[i], right_max[i]) - height[i]);
         }
-
-        return water;
+        return ans;
     }
 };
-
-// class Solution {
-// public:
-//     int trap(vector<int>& height) {
-//         // prefix max suffix max.. can be one approach.. for an index get the left max annd right max
-//         preMax 
-//     }
-// };
-
-// class Solution {
-// public:
-//     int trap(vector<int>& height) {
-//         // prefix max suffix max.. can be one approach.. for an index get the left max annd right max
-//         preMax 
-//     }
-// };
